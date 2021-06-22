2f10bcb74605c4da7c1ee28656ada150903fdbad
Ian Hornik <yiliansource@gmail.com>
2021-06-22 12:32:21 +0200

    Added generated API, closes #42 (#65)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,26 @@ module.exports = {
     favicon: "img/logo.svg",
     organizationName: "yiliansource",
     projectName: "party-js",
-    plugins: ["docusaurus-plugin-sass"],
+    plugins: [
+        "docusaurus-plugin-sass",
+        [
+            "docusaurus-plugin-typedoc",
+            {
+                entryPoints: ["../src"],
+                tsconfig: "../tsconfig.json",
+
+                readme: "none",
+                includeVersion: true,
+                excludePrivate: true,
+                excludeProtected: true,
+                excludeInternal: true,
+
+                sidebar: {
+                    sidebarFile: null,
+                },
+            },
+        ],
+    ],
     themeConfig: {
         image: "img/banner.png",
         metadatas: [
@@ -37,10 +56,16 @@ module.exports = {
             },
             items: [
                 {
-                    to: "docs/",
-                    activeBasePath: "docs",
-                    label: "Docs",
+                    type: "doc",
+                    docId: "quick-start",
                     position: "left",
+                    label: "Docs",
+                },
+                {
+                    type: "doc",
+                    docId: "api/index",
+                    position: "left",
+                    label: "API",
                 },
                 {
                     href: "https://github.com/yiliansource/party-js",
